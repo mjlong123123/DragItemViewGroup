ed49efe2046b7bbe79b89abff5acbaa04e969476
dragon <chen_jiulong@navercorp.com>
2018-01-16 21:45:09 +0800

    update

--- app/src/main/java/com/dragon/scrollitemviewgroup/DragItemViewGroup.java
+++ app/src/main/java/com/dragon/scrollitemviewgroup/DragItemViewGroup.java
@@ -2,6 +2,7 @@ package com.dragon.scrollitemviewgroup;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,22 +11,77 @@ import android.view.ViewGroup;
  */
 
 public class DragItemViewGroup extends ViewGroup {
-	public DragItemViewGroup(Context context) {
-		super(context);
-	}
+    private TouchHelper touchHelper;
 
-	public DragItemViewGroup(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public DragItemViewGroup(Context context) {
+        super(context);
+        init();
+    }
 
-	@Override
-	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		if (getChildCount() != 2) {
-			throw new RuntimeException("child count error!");
-		}
-		View childView = getChildAt(0);
-		childView.layout(getPaddingLeft(), getPaddingTop(), getPaddingLeft() + childView.getMeasuredWidth(), getPaddingTop() + childView.getMeasuredHeight());
-		childView = getChildAt(1);
-		childView.layout(getRight(), getPaddingTop(), getRight() + childView.getMeasuredWidth(), getPaddingTop() + childView.getMeasuredHeight());
-	}
+    public DragItemViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        touchHelper = new TouchHelper(this);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (getChildCount() != 2) {
+            throw new RuntimeException("child count error!");
+        }
+
+        View childView = getChildAt(0);
+        measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+        childView = getChildAt(1);
+        measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (getChildCount() != 2) {
+            throw new RuntimeException("child count error!");
+        }
+        View childView = getChildAt(0);
+        childView.layout(0, 0, childView.getMeasuredWidth(), childView.getMeasuredHeight());
+        childView = getChildAt(1);
+        childView.layout(getRight(), 0, getRight() + childView.getMeasuredWidth(), childView.getMeasuredHeight());
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (touchHelper.onInterceptTouchEvent(ev)) {
+            return true;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (touchHelper.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+//
+//    @Override
+//    public void computeScroll() {
+//        touchHelper.computeScroll();
+//    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    }
 }
---
+++ app/src/main/java/com/dragon/scrollitemviewgroup/TouchHelper.java
@@ -0,0 +1,114 @@
+package com.dragon.scrollitemviewgroup;
+
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * Created by Administrator on 2018/1/16.
+ */
+
+public class TouchHelper {
+
+    float downX = 0;
+
+    ViewGroup view;
+
+
+    int touchSlop;
+    boolean dragFlag;
+    float lastX = 0;
+
+    Scroller scroller;
+
+    int direct = 0;
+
+    Handler handler = new Handler();
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if(!scroller.isFinished()){
+                computeScroll();
+            }
+        }
+    };
+
+    public TouchHelper(ViewGroup view) {
+        this.view = view;
+        scroller = new Scroller(view.getContext());
+        touchSlop = ViewConfiguration.get(view.getContext()).getScaledTouchSlop();
+    }
+
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        int action = ev.getActionMasked();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                scroller.forceFinished(true);
+                downX = ev.getX();
+                lastX = downX;
+                dragFlag = false;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float currentX = ev.getX();
+                lastX = currentX;
+                float diffX = Math.abs(currentX - downX);
+                if (diffX > touchSlop) {
+                    dragFlag = true;
+                    view.getParent().requestDisallowInterceptTouchEvent(true);
+                }
+                break;
+        }
+        return dragFlag;
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE: {
+                float currentX = event.getX();
+                float offsetX = currentX - lastX;
+                lastX = currentX;
+                View childView = view.getChildAt(1);
+                if (offsetX + childView.getLeft() > view.getRight()) {
+                    offsetX = view.getRight() - childView.getLeft();
+                } else if (offsetX + childView.getRight() < view.getRight()) {
+                    offsetX = view.getRight() - childView.getRight();
+                }
+                childView.offsetLeftAndRight((int) offsetX);
+                childView = view.getChildAt(0);
+                childView.offsetLeftAndRight((int) offsetX);
+
+            }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL: {
+                float currentX = event.getX();
+
+                direct = (int) (currentX - downX);
+                View childView = view.getChildAt(1);
+                int dx = direct > 0 ? view.getRight() - childView.getLeft() : view.getRight() - childView.getRight();
+                scroller.startScroll(childView.getLeft(), 0, dx, 0);
+                handler.post(runnable);
+            }
+                break;
+        }
+        return true;
+    }
+    public void computeScroll() {
+        if(scroller.computeScrollOffset()){
+            int x = scroller.getCurrX();
+            View childView = view.getChildAt(1);
+            int offset = x - childView.getLeft();
+            childView.offsetLeftAndRight(offset);
+            childView = view.getChildAt(0);
+            childView.offsetLeftAndRight(offset);
+            handler.post(runnable);
+        }
+    }
+}
